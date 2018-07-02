b266618907c432f5cf2d1661be4274694d6ae3d0
Danielle Ivy <danielleivy@Danielles-MacBook-Pro.local>
2018-07-01 21:19:33 -0400

    add pages, create database class and initialize

---
+++ admin/includes/database.php
@@ -0,0 +1,14 @@
+<?php
+require_once("config.php");
+class Database {
+    private $connection;
+    public function open_db_connection() {
+        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        if (mysqli_connect_errno()) {
+            die("Database connection FAILED: " . mysqli_error());
+        }
+    }
+} 
+$database = new Database();
+$database->open_db_connection();
+?>
