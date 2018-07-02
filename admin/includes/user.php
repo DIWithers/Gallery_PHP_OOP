c70070ca2d4101defb9e74d3da009e0ddcbb1ce1
Danielle Ivy <danielleivy@Danielles-MacBook-Pro.local>
2018-07-01 23:06:41 -0400

    find user by id

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -1,9 +1,13 @@
 <?php
-
-class User {
-    public function find_all_users() {
-        global $database;
-        return $database->query("SELECT * FROM users");
+    class User {
+        public static function find_all_users() {
+            global $database;
+            return $database->query("SELECT * FROM users");
+        }
+        public static function find_user_by_id($id) {
+            global $database;
+            $result = $database->query("SELECT * FROM users WHERE id={$id} LIMIT 1");
+            return mysqli_fetch_array($result);
+        }
     }
-}
 ?>
