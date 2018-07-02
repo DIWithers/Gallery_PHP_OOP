77343b0607c9cc5e5005c2c628354bc9c8ff2766
Danielle Ivy <danielleivy@Danielles-MacBook-Pro.local>
2018-07-01 23:34:10 -0400

    refactor user methods

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -1,13 +1,30 @@
 <?php
     class User {
         public static function find_all_users() {
-            global $database;
-            return $database->query("SELECT * FROM users");
+            return self::run_query("SELECT * FROM users");
         }
         public static function find_user_by_id($id) {
-            global $database;
-            $result = $database->query("SELECT * FROM users WHERE id={$id} LIMIT 1");
+            $result = self::run_query("SELECT * FROM users WHERE id={$id} LIMIT 1");
             return mysqli_fetch_array($result);
         }
+        public static function run_query($query) {
+            global $database;
+            return $database->query($query); 
+        }
     }
+?>
+
+
+<?php
+    // class User {
+    //     public static function find_all_users() {
+    //         global $database;
+    //         return $database->query("SELECT * FROM users");
+    //     }
+    //     public static function find_user_by_id($id) {
+    //         global $database;
+    //         $result = $database->query("SELECT * FROM users WHERE id={$id} LIMIT 1");
+    //         return mysqli_fetch_array($result);
+    //     }
+    // }
 ?>
