ec5f5c471c1acee0e2ae955e771e725d019ead9f
Danielle Ivy <danielleivy@Danielles-MacBook-Pro.local>
2018-07-03 11:21:11 -0400

    replace deprecated function

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -1,6 +1,6 @@
 <?php 
     // checks for file inclusion and corrects if forgotten
-    function __autoload($class) {
+    function classAutoLoader($class) {
         $class = strtolower($class);
         $path = "includes/{$class}.php";
         if(file_exists($path)) {
@@ -8,5 +8,6 @@
         }
         else die ("ERROR : The filename {$class}.php was not found.");
     }
+    spl_autoload_register("classAutoLoader");
 
 ?>
