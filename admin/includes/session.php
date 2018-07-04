3f137f99a6e2b35490d4b7a35eeab10cb1e7dad1
Danielle Ivy <danielleivy@Danielles-MacBook-Pro.local>
2018-07-03 20:25:12 -0400

    message methods

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -2,10 +2,12 @@
     class Session {
         private $signed_in = false;
         public $user_id;
+        public $message;
 
         function __construct() {
             session_start();
             $this->check_login();
+            $this->check_message();
         }
         private function check_login() {
             if (isset($_SESSION['user_id'])) {
@@ -31,6 +33,18 @@
             unset($this->user_id);
             $this->signed_in = false;
             session_destroy();
+        }
+        public function message($msg="") {
+            if(!empty($msg)) {
+                $_SESSION['message'] = $msg;
+            }
+            else return $this->message;
+        }
+        private function check_message() {
+            if (isset($_SESSION['message'])) {
+                $this->message = $_SESSION['message'];
+            }
+            else $this->message = "";
         }
     }
     $session = new Session();
