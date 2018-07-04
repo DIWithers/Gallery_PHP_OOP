8ccad831ca68f330b191606effbf035353faea19
Danielle Ivy <danielleivy@Danielles-MacBook-Pro.local>
2018-07-03 20:53:30 -0400

    error message with incorrect login

--- admin/login.php
+++ admin/login.php
@@ -8,16 +8,19 @@
         $password = trim($_POST['password']);
         $user_found = User::verify_user($username, $password);
         if ($user_found) {
+            $messsage = "Found!";
             $session->login($user_found);
             redirect("index.php");
         }
         else {
             $messsage = "Your username or password is incorrect";
+            $username = "";
+            $password = "";
         }
     }
 ?>
 <div class="col-md-4 col-md-offset-3">
-    <h4 class="bg-danger"><?php echo $message; ?></h4>
+    <h4 class="bg-danger"><?php echo $messsage; ?></h4>
     <form id="login-id" action="" method="post">
         <div class="form-group">
             <label for="username">Username</label>
