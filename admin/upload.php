d522156d0a63c13db7ae50eef0403529a4feb280
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-07-22 11:19:04 -0400

    Create upload form

--- admin/upload.php
+++ admin/upload.php
@@ -1,41 +1,36 @@
 <?php include("includes/header.php"); ?>
+<?php 
+    if (!$session->is_signed_in()) {
+        redirect("login.php");
+    } 
+?>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <?php include("includes/top_nav.php") ?>
+    <?php include("includes/side_nav.php") ?>
+</nav>
 
-        <!-- Navigation -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <?php include("includes/top_nav.php") ?>
-            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-            <?php include("includes/side_nav.php") ?>
-            <!-- /.navbar-collapse -->
-        </nav>
-
-        <div id="page-wrapper">
-
-            <div class="container-fluid">
-
-            <!-- Page Heading -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">
-                        UPLOAD
-                        <small>Subheading</small>
-                    </h1>
-                    <ol class="breadcrumb">
-                        <li>
-                            <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                        </li>
-                        <li class="active">
-                            <i class="fa fa-file"></i> Blank Page
-                        </li>
-                    </ol>
+<div id="page-wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    UPLOAD
+                    <small>Subheading</small>
+                </h1>
+                <div class="col-md-6">
+                    <form action="upload.php" method="post" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <input type="text" name="title" class="form-control"></input>
+                        </div>
+                        <div class="form-group">
+                            <input type="file" name="file_upload"></input>
+                        </div>
+                        <input type="submit" name="submit"></input>
+                    </form>
                 </div>
             </div>
-            <!-- /.row -->
-
-            </div>
-            <!-- /.container-fluid -->
-
         </div>
-        <!-- /#page-wrapper -->
+    </div>
+</div>
 
-  <?php include("includes/footer.php"); ?>
+<?php include("includes/footer.php"); ?>
