4084cd9c2737235769b32dea7d7c973c4696c4c7
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-07-24 14:42:42 -0400

    display users

--- admin/users.php
+++ admin/users.php
@@ -1,41 +1,56 @@
 <?php include("includes/header.php"); ?>
+<?php if (!$session->is_signed_in()) {redirect("login.php");} ?>
 
-        <!-- Navigation -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <?php include("includes/top_nav.php") ?>
-            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-            <?php include("includes/side_nav.php") ?>
-            <!-- /.navbar-collapse -->
-        </nav>
+<?php 
+    $users = User::find_all();
+?>
 
-        <div id="page-wrapper">
-
-            <div class="container-fluid">
-
-            <!-- Page Heading -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <?php include("includes/top_nav.php") ?>
+        <?php include("includes/side_nav.php") ?>
+    </nav>
+    <div id="page-wrapper">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
                     <h1 class="page-header">
                         USERS
                         <small>Subheading</small>
                     </h1>
-                    <ol class="breadcrumb">
-                        <li>
-                            <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                        </li>
-                        <li class="active">
-                            <i class="fa fa-file"></i> Blank Page
-                        </li>
-                    </ol>
+                    <div class="col-md-12">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Photo</th>
+                                    <th>Username</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach($users as $user) : ?>
+                                <tr>
+                                    <td><?php echo $user->id; ?> </td>
+
+                                    <td><img class="admin-user-thumbnail" src="<?php echo $user->user_image ?>" alt="<?php echo $user->title; ?>"/></td>
+                                    <td><?php echo $user->username; ?>
+                                        <div class="actions_link">
+                                            <a href="delete_user.php?id=<?php echo $user->id ?>">Delete</a>
+                                            <a href="edit_user.php?id=<?php echo $user->id ?>">Edit</a>
+                                            <a href="view_user.php?id=<?php echo$user->id ?>">View</a>
+                                        </div>
+                                    </td>
+                                    <td><?php echo $user->first_name; ?></td>
+                                    <td><?php echo $user->last_name; ?></td>
+                                </tr>
+                            <?php endforeach ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-            <!-- /.row -->
-
-            </div>
-            <!-- /.container-fluid -->
-
         </div>
-        <!-- /#page-wrapper -->
-
+    </div>
   <?php include("includes/footer.php"); ?>
