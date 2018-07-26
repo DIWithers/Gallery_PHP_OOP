7dd335d7e01132ddcfe69d7f788aaa01e5962e9e
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-07-26 16:47:04 -0400

    Display photos on index

--- index.php
+++ index.php
@@ -1,30 +1,16 @@
 <?php include("includes/header.php"); ?>
-
-
-        <div class="row">
-
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
-
-    
-            
-          
-         
-
-            </div>
-
-
-
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-            
-                 <?php include("includes/sidebar.php"); ?>
-
-
-
+<?php $photos = Photo::find_all(); ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="thumbnails row">
+            <?php foreach($photos as $photo) : ?>
+                <div class="col-xs-6 col-md-3">
+                    <a href="" class="thumbnail">
+                        <img src="admin/<?php echo $photo->image_path();?>" class="home-page-photo img-responsive" alt="">
+                    </a>
+                </div>
+            <?php endforeach ?>
         </div>
-        <!-- /.row -->
-
-        <?php include("includes/footer.php"); ?>
+    </div>
+</div>
+<?php include("includes/footer.php"); ?>
